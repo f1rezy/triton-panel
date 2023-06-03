6f5f548f029c179bc1da906dd1493c4789f62334
Daniil Litvinenko <danil.fire@bk.ru>
2023-06-04 02:00:29 +0700

    test commit

--- app/endpoints/triton.py
+++ app/endpoints/triton.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import jsonify, request
 from flask_jwt_extended import jwt_required
+import tritonclient.http as tc
 
 from database import db
 from models import *
@@ -11,16 +12,23 @@ bp = Blueprint("triton", __name__)
 @bp.route("/models", methods=["GET"])
 @jwt_required()
 def get_triton_loaded_models():
-    return jsonify([{model.data} for model in db.session.query(Model).all()])
+    return jsonify([
+        {
+            "model_id": version.model_version.model_id,
+            "model_name": version.model_version.model.name,
+            "version_id": version.model_version_id,
+            "version_name": version.model_version.name,
+            "triton_loaded_version_id": version.id
+        } for version in db.session.query(TritonLoaded).all()])
 
 
-@bp.route("/model/<id>", methods=["POST"])
+@bp.route("/model_version/<id>", methods=["POST"])
 @jwt_required()
 def add_model_to_triton(id: str):
     return jsonify({"status": True})
 
 
-@bp.route("/model/<id>", methods=["DELETE"])
+@bp.route("/triton_loaded_version/<id>", methods=["DELETE"])
 @jwt_required()
 def delete_model_from_triton(id: str):
     return jsonify({"status": True})
