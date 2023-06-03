2fcbf1947a05c82132689b0ddae30c1f17cad9cd
Daniil Litvinenko <danil.fire@bk.ru>
2023-06-04 05:56:34 +0700

    coooomit

--- app/endpoints/triton.py
+++ app/endpoints/triton.py
@@ -32,14 +32,14 @@ def add_model_to_triton(id: str):
         jsonify({"status": False}), 404
 
     source_path = os.path.abspath("models_onnx") + "/" + version.model.name + "/" + version.name
-    destination_path = "/home/model_repository"
+    destination_path = "/home/model_repository/" + version.model.name
     shutil.copytree(source_path, destination_path)
 
     triton_loaded = TritonLoaded(model_version_id=version.id)
     db.session.add(triton_loaded)
     db.session.commit()
 
-    return jsonify({"status": True}), 200
+    return jsonify({"status": True, "path": source_path}), 200
 
 
 @bp.route("/model/<id>", methods=["DELETE"])
