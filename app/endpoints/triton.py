bda9647bc4807d715bcf5f21bb805074d88caae2
Daniil Litvinenko <danil.fire@bk.ru>
2023-06-04 16:25:08 +0700

    test commit

--- app/endpoints/triton.py
+++ app/endpoints/triton.py
@@ -1,4 +1,3 @@
-import shutil
 import tritonclient.http as httpclient
 
 from flask import Blueprint
@@ -31,20 +30,23 @@ def add_model_to_triton(id: str):
     if not version:
         return jsonify({"status": False}), 404
 
-    triton_client = httpclient.InferenceServerClient(url="localhost:8000",
-                                                     verbose=False)
-    model_name = version.model.name
+    triton_client = httpclient.InferenceServerClient(url="localhost:8000", verbose=False)
+    model_name = "simple"
 
     triton_client.load_model(model_name)
     if not triton_client.is_model_ready(model_name):
         return jsonify({"status": False})
 
-    config = open(f"models_onnx/{model_name}/{version.name}/config.pbtxt", mode="r").read()
+    # config = open(f"models_onnx/{model_name}/{version.name}/config.pbtxt", mode="r").read()
+    config = "{\"max_batch_size\":\"16\"}"
     triton_client.load_model(model_name, config=config)
+    if triton_client.is_model_ready(model_name):
+        return jsonify({"dadadada": True})
 
-    triton_loaded = TritonLoaded(model_version_id=version.id)
-    db.session.add(triton_loaded)
-    db.session.commit()
+    # triton_loaded = TritonLoaded(model_version_id=version.id)
+    # db.session.add(triton_loaded)
+    # db.session.commit()
+    triton_client.unload_model(model_name)
 
     return jsonify({"status": True}), 200
 
