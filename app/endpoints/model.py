738849161a2ffe499988a2adaacac097843f710f
Daniil Litvinenko <danil.fire@bk.ru>
2023-06-04 17:49:13 +0700

    Commit

--- app/endpoints/model.py
+++ app/endpoints/model.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-import tritonclient.grpc as grpcclient
+import tritongrpcclient as grpcclient
 
 from flask import Blueprint, send_file, after_this_request
 from flask import jsonify, request
