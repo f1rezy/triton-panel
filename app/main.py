e5d42e18989703fca9b4d1ade8a0d60f47152e22
IT-Cube <user@note-corei5-18.akiac.ru>
2023-06-26 16:27:19 +0700

    fix-cors-config

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from starlette.middleware.cors import CORSMiddleware
 from app.api.api import api_router
 from app.core.config import settings
 
-
 app = FastAPI(
     title=settings.PROJECT_NAME
 )
@@ -19,7 +18,6 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
 
 app.include_router(api_router, prefix=settings.API_STR)
 
