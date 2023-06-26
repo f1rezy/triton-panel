b3ae26246695533edad5a61ea60c8a22a1778e83
IT-Cube <user@note-corei5-18.akiac.ru>
2023-06-26 16:37:40 +0700

    fix-cors-config

--- app/main.py
+++ app/main.py
@@ -16,8 +16,7 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
         allow_credentials=True,
         allow_methods=["*"],
-        allow_headers=["*"],
-        expose_headers=["*"]
+        allow_headers=["*"]
     )
 
 app.include_router(api_router, prefix=settings.API_STR)
