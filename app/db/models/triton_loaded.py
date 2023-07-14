82be24e234dd3cff47081aa8af95e431cb125312
Daniil Litvinenko <danil.fire@bk.ru>
2023-07-14 22:10:34 +0700

    123

--- app/db/models/triton_loaded.py
+++ app/db/models/triton_loaded.py
@@ -8,6 +8,6 @@ from app.db.models.base_class import Base
 class TritonLoaded(Base):
     __tablename__ = "triton_loaded"
 
-    version_id = Column("model_version_id", ForeignKey("versions.id"), nullable=False)
+    version_id = Column("version_id", ForeignKey("versions.id"), nullable=False)
 
     version = relationship("Version", back_populates="triton_loaded_version", lazy='selectin')
