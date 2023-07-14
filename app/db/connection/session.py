af703d55b2115ec1eff59e5ccd9e631b8de6c61a
Daniil Litvinenko <danil.fire@bk.ru>
2023-07-14 21:07:11 +0700

    Commit

--- app/db/connection/session.py
+++ app/db/connection/session.py
@@ -22,6 +22,7 @@ class SessionManager:
         return sessionmaker(self.engine, class_=AsyncSession, expire_on_commit=False)
 
     def refresh(self) -> None:
+        print(settings.SQLALCHEMY_DATABASE_URI)
         self.engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI, echo=False, future=True)
 
 
